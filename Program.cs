cb05e580ed6edd5412e3ec721c1cb2dc213f1edb
Steven Palmer <83670458+stevewpalmer@users.noreply.github.com>
2023-12-24 17:41:06 +0000

    Add project files and day 1 solution

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int total1 = 0;
+int total2 = 0;
+
+string [] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+foreach (string line in File.ReadAllLines("day1.txt")) {
+
+    string newline = line;
+    for (int index = 0; index < numbers.Length; index++) {
+        newline = newline.Replace(numbers[index], $"{numbers[index]}{index}{numbers[index]}");
+    }
+    total1 += Convert.ToInt32($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");
+    total2 += Convert.ToInt32($"{newline.First(char.IsDigit)}{newline.Last(char.IsDigit)}");
+}
+Console.WriteLine($"Puzzle 1 answer : Total = {total1}");
+Console.WriteLine($"Puzzle 1 answer : Total = {total2}");
