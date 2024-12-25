da18776af7025e302a33a15bc033372775f17b15
Steve Palmer <steve@stevewpalmer.uk>
2024-12-25 16:56:38 +0000

    Day 4, part 1 solution.

---
+++ Day4/Program.cs
@@ -0,0 +1,12 @@
+﻿int points = 0;
+foreach (string line in File.ReadAllLines("puzzle.txt")) {
+    string [] parts = line.Substring(line.IndexOf(':') + 1).Split('|');
+    int [] winningNumbers = Array.ConvertAll(parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+    int [] cardNumbers = Array.ConvertAll(parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+    int count = cardNumbers.Count(c => winningNumbers.Contains(c));
+    if (count > 0) {
+        points += 1 << (count - 1);
+    }
+}
+
+Console.WriteLine($"Part 1 answer: {points}");
