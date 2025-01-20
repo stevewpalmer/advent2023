3ef260cf49976953b95d7dd391eedeb660a2a1ac
steve <steve@stevewpalmer.uk>
2025-01-20 22:24:39 +0000

    Day 15 part 1 answer.

---
+++ Day15/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] parts = File.ReadAllText("puzzle.txt").Split(',');
+
+long answer1 = parts.Aggregate<string, long>(0, (current, part) => current + Hash(part));
+Console.WriteLine($"Part 1 answer: {answer1}");
+return;
+
+int Hash(string s) => s.Aggregate(0, (current, ch) => (current + ch) * 17 % 256);
