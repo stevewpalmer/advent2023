2cd1e3372d074f1f57536b180d864dbd8775decf
steve <steve@stevewpalmer.uk>
2025-01-25 05:11:41 +0000

    Vastly improved day 18 solution.

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -1,39 +1,31 @@
-﻿using System.Drawing;
-
-List<Point> points = [];
-
-int x = 0;
-int y = 0;
-
-points.Add(new Point(x, y));
-foreach (string line in File.ReadLines("puzzle.txt")) {
-    string[] parts = line.Split(' ');
-    int length = int.Parse(parts[1]);
-    switch (parts[0][0]) {
-        case 'R': x += length; break;
-        case 'L': x -= length; break;
-        case 'U': y -= length; break;
-        case 'D': y += length; break;
-    }
-    points.Add(new Point(x, y));
-}
-long area = 0;
-long perm = 0;
-for (int i = 0; i < points.Count - 1; i++) {
-    int px1 = points[i].X;
-    int py1 = points[i].Y;
-    int px2 = points[i + 1].X;
-    int py2 = points[i + 1].Y;
-    area += px1 * py2 - py1 * px2;
-    perm += GCD(Math.Abs(px2 - px1), Math.Abs(py2 - py1));
-}
-long answer1 = area / 2 + perm / 2 + 1;
-Console.WriteLine($"Part 1 answer: {answer1}");
+﻿Console.WriteLine($"Part 1 answer: {SolveArea(1)}");
+Console.WriteLine($"Part 1 answer: {SolveArea(2)}");
 return;
 
-int GCD(int val1, int val2) {
-    while (val2 != 0) {
-        (val1, val2) = (val2, val1 % val2);
+long SolveArea(int part) {
+    long x = 0;
+    long y = 0;
+
+    long area = 0;
+    long perimeter = 0;
+
+    long px = 0;
+    long py = 0;
+    foreach (string line in File.ReadLines("puzzle.txt")) {
+        string[] parts = line.Split(' ');
+        long hexNumber = Convert.ToInt64(parts[2].Substring(2, parts[2].Length - 3), 16);
+        long length = part == 1 ? Convert.ToInt32(parts[1]) : hexNumber / 16;
+        char direction = part == 1 ? parts[0][0] : "RDLU"[(int)hexNumber % 16];
+        switch (direction) {
+            case 'R': x += length; break;
+            case 'L': x -= length; break;
+            case 'U': y -= length; break;
+            case 'D': y += length; break;
+        }
+        area += px * y - py * x;
+        perimeter += length;
+        px = x;
+        py = y;
     }
-    return val1;
+    return area / 2 + perimeter / 2 + 1;
 }
