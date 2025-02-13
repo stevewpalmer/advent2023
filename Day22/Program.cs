76e2ce524037e5447bbfbcb9eb33026c17efa68e
steve <steve@stevewpalmer.uk>
2025-02-13 08:07:43 +0000

    Remove vectors.

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Drawing;
-using System.Numerics;
-using Block = (int index, System.Numerics.Vector3 pos, System.Numerics.Vector3 size);
+using Block = (int index, int x, int y, int z, int height, System.Drawing.Rectangle rect);
 
 List<Block> blocks = [];
 List<Block> bases = [];
@@ -10,26 +9,22 @@ Dictionary<int, List<int>> supports = [];
 int blockIndex = 0;
 foreach (string input in File.ReadAllLines("puzzle.txt")) {
     string[] parts = input.Split('~');
-    int[] corner1 = parts[0].Split(',').Select(int.Parse).ToArray();
-    int[] corner2 = parts[1].Split(',').Select(int.Parse).ToArray();
-    Vector3 p = new(corner1[0], corner1[1], corner1[2]);
-    Vector3 r = new(corner2[0] - corner1[0] + 1, corner2[1] - corner1[1] + 1, corner2[2] - corner1[2] + 1);
-    blocks.Add((blockIndex, p, r));
+    int[] c1 = parts[0].Split(',').Select(int.Parse).ToArray();
+    int[] c2 = parts[1].Split(',').Select(int.Parse).ToArray();
+    Rectangle rect = new(c1[0], c1[1], c2[0] - c1[0] + 1, c2[1] - c1[1] + 1);
+    blocks.Add((blockIndex, c1[0], c1[1], c1[2], c2[2] - c1[2] + 1, rect));
     blockIndex++;
 }
 
-foreach (Block b in blocks.OrderBy(b => b.pos.Z)) {
-    Vector3 restingPosition = new(b.pos.X, b.pos.Y, 1);
-    Rectangle bRect = new((int)b.pos.X, (int)b.pos.Y, (int)b.size.X, (int)b.size.Y);
-
-    foreach (Block nb in bases.OrderByDescending(nb => nb.pos.Z + nb.size.Z)) {
-        Rectangle nbRect = new((int)nb.pos.X, (int)nb.pos.Y, (int)nb.size.X, (int)nb.size.Y);
-        if (nbRect.IntersectsWith(bRect)) {
-            float nbTop = nb.pos.Z + nb.size.Z;
-            if (nbTop < restingPosition.Z) {
+foreach (Block b in blocks.OrderBy(b => b.z)) {
+    int maxZ = 1;
+    foreach (Block nb in bases.OrderByDescending(nb => nb.z + nb.height)) {
+        if (nb.rect.IntersectsWith(b.rect)) {
+            int nbTop = nb.z + nb.height;
+            if (nbTop < maxZ) {
                 continue;
             }
-            restingPosition.Z = nbTop;
+            maxZ = nbTop;
             if (!supportedBy.ContainsKey(b.index)) {
                 supportedBy[b.index] = [];
             }
@@ -40,7 +35,7 @@ foreach (Block b in blocks.OrderBy(b => b.pos.Z)) {
             supports[nb.index].Add(b.index);
         }
     }
-    bases.Add((b.index, restingPosition, b.size));
+    bases.Add((b.index, b.x, b.y, maxZ, b.height, b.rect));
 }
 long answer1 = 0;
 for (int c = 0; c < blockIndex; c++) {
